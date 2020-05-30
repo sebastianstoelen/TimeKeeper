bc8db59a31fc9b8e3f971e818b4a9b76e4f0ddbd
sstoelen <sebastian.stoelen@ae.be>
2020-05-30 15:01:32 +0200

    make function async again

--- TimeKeeper/StartTimer.cs
+++ TimeKeeper/StartTimer.cs
@@ -1,19 +1,17 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace TimeKeeper
 {
     public static class StartTimer
     {
         [FunctionName("StartTimer")]
-        public static IActionResult Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
